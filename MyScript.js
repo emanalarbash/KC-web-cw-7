b9fc045014bba49e3afa39811ee6bd9db94f1d3b
emanalarbash <emanm@arbash.co>
2022-08-19 22:27:30 +0300

    doooone and excuse for be late

--- MyScript.js
+++ MyScript.js
@@ -7,15 +7,22 @@ function BMI(weight=70,height=1.7){
 console.log(BMI(70, 1.7));
 
 function Status(bmi) {
+    let div = document.getElementById("result")
+
     if(bmi< 18.5) {
+        div.style.color="orange"
         return  "لديك نقص في الوزن" ;   
 
     } else if (bmi >= 18.5 && bmi < 25) {
+        div.style.color = "green";
         return "وزنك صحي" ;
 } else {   
+    div.style.color = "red";
       return  "لديك زيادة في الوزن";
     }
 }
+
+
 function calculate() {
 
 let weight = document.getElementById("weight").value;
@@ -32,7 +39,7 @@ div.innerText = bmi + " == " + desc;
 
 }
 
-//function BMI(p1, p2=7)
+//function BMI(weight, height)
 console.log(BMI());
 console.log(BMI());
 console.log(BMI());
